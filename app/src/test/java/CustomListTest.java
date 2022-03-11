b058af103ec2bf435c5b122796f3537b661021fb
Aaron383 <aaronboyd385@gmail.com>
2022-03-10 19:35:02 -0700

    Watch test fail

--- app/src/test/java/CustomListTest.java
+++ app/src/test/java/CustomListTest.java
@@ -1,10 +1,13 @@
-import static org.junit.Assert.assertEquals;
+
+
+import static org.junit.jupiter.api.Assertions.*;
 
 import com.example.simpleparadox.listycity.City;
 import com.example.simpleparadox.listycity.CustomList;
 
-import org.junit.Before;
-import org.junit.Test;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
 
@@ -12,7 +15,7 @@ public class CustomListTest {
 
     private CustomList list;
 
-    @Before
+    @BeforeAll
     public void createList() {
         list = new CustomList(null, new ArrayList<City>());
     }
@@ -24,5 +27,14 @@ public class CustomListTest {
         list.addCity(new City("Halifax", "NS"));
         assertEquals(listSize+1, list.getCount());
     }
+
+    @Test
+    public void hasCityTest() {
+        City city = new City("Halifax", "NS");
+        list.addCity(city);
+        assertTrue(list.hasCity(city));
+    }
+
+
 
 }
